cbdfc1491381fd5378a554b82217fef0cf76ced1
“mubarak-bunza” <mubarakabubakar227@gmail.com>
2021-02-18 15:12:26 +0100

    edited settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '1w(qw%o5*#(!i$yjtb8*ej)@1bu(tknrsez4qja(kbdt$fguep'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
