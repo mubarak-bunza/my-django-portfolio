6fcb5bcd5af91ee0e88e4d2226ffa05df29663a4
“mubarak-bunza” <mubarakabubakar227@gmail.com>
2021-02-20 12:39:29 +0100

    congifured heroku db

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+from decouple import config
 # import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '1w(qw%o5*#(!i$yjtb8*ej)@1bu(tknrsez4qja(kbdt$fguep'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['mab-portfolio.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,16 +79,27 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': 'portfolio_db',
-        'USER': 'mubarakabubakar',
-        'PORT': '5432'
+print(DEBUG)
+if not DEBUG:
+    DATABASES = {
+        # 'default': {
+        #     dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600),
+        #     'ENGINE': 'django.db.backends.postgresql',
+        # }
+        'dafault': dj_database_url.config(
+            dafault=config('DATABASE_URL')
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            # 'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': 'portfolio_db',
+            'USER': 'mubarakabubakar',
+            'PORT': '5432'
+        }
+    }
 
 
 # Password validation
